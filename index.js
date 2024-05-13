fb143866db3306eb0a7680464b59c12facc0ed74
Mehulk20 <144333451+Mehulk20@users.noreply.github.com>
2024-05-13 23:43:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const drumBtns = document.querySelectorAll(".drum");
+const audioFile = new Audio("./sounds/crash.mp3");
+const bgImg = document.createElement("img");
+drumBtns.forEach((btn, key) => {
+  const bgImg = document.createElement("img");
+  bgImg.setAttribute("src", `./images/tom${key + 1}.png`);
+  btn.prepend(bgImg);
+  btn.addEventListener("mousedown", function () {
+    const audioFile = new Audio(`./sounds/tom-${key + 1}.mp3`);
+    btn.style.backgroundColor = "yellow";
+    audioFile.play();
+  });
+  btn.addEventListener("mouseup", function () {
+    btn.style.backgroundColor = "white";
+  });
+});
